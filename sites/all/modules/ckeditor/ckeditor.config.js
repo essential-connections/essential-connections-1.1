323207635f313e39bedd0726b47505cdc0310088
s <s@lodhie.org>
2011-05-09 20:26:04 -0700

    ok

--- sites/all/modules/ckeditor/ckeditor.config.js
+++ sites/all/modules/ckeditor/ckeditor.config.js
@@ -115,21 +115,21 @@ CKEDITOR.editorConfig = function(config) {
   if (Drupal.ckeditorCompareVersion('3.1')) {
     CKEDITOR.plugins.addExternal('drupalbreaks', Drupal.settings.ckeditor.module_path + '/plugins/drupalbreaks/');
   }
-  if (Drupal.settings.ckeditor.linktocontent_menu) {
+  //if (Drupal.settings.ckeditor.linktocontent_menu) {
     CKEDITOR.plugins.addExternal('linktomenu', Drupal.settings.ckeditor.module_path + '/plugins/linktomenu/');
     Drupal.settings.ckeditor.linktomenu_basepath = Drupal.settings.basePath;
-  }
-  if (Drupal.settings.ckeditor.linktocontent_node) {
+  //}
+  //if (Drupal.settings.ckeditor.linktocontent_node) {
     CKEDITOR.plugins.addExternal('linktonode', Drupal.settings.ckeditor.module_path + '/plugins/linktonode/');
     Drupal.settings.ckeditor.linktonode_basepath = Drupal.settings.basePath;
-  }
+  //}
 
   // 'MediaEmbed' plugin. To enable it, uncomment lines below and add 'MediaEmbed' button to selected toolbars.
   config.extraPlugins += (config.extraPlugins ? ',mediaembed' : 'mediaembed' );
   CKEDITOR.plugins.addExternal('mediaembed', Drupal.settings.ckeditor.module_path + '/plugins/mediaembed/');
 
   // 'IMCE' plugin. If IMCE module is enabled, you may uncomment lines below and add an 'IMCE' button to selected toolbar.
-  //config.extraPlugins += (config.extraPlugins ? ',imce' : 'imce' );
-  //CKEDITOR.plugins.addExternal('imce', Drupal.settings.ckeditor.module_path + '/plugins/imce/');
+  config.extraPlugins += (config.extraPlugins ? ',imce' : 'imce' );
+  CKEDITOR.plugins.addExternal('imce', Drupal.settings.ckeditor.module_path + '/plugins/imce/');
 };
 
